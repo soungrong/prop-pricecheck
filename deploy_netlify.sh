d2af5c22ea8b471a68ff66c05984b1e42e487cde
Soung Rong <soungcoding@gmail.com>
2019-07-22 18:52:26 +0800

    Complete netlify deploy script

--- deploy_netlify.sh
+++ deploy_netlify.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
 sh ./price_app/scripts/pre_deploy_netlify.sh
+
+netlify deploy --dir=./price_app/build
+
+deploy_to_production () {
+    echo "Deploy to production? y/n"
+    read userinput
+    if [ "$userinput" = "y" ]; then
+        echo "Deploying to production."
+        netlify deploy --prod
+        elif [ "$userinput" = "n" ]; then
+            echo "Not deploying to production."
+        else
+            echo "Invalid input."
+            deploy_to_production
+        fi
+}
+
+# run container_cleanup function
+deploy_to_production
