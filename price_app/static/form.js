8009cfeb715eb93e931307461b3ee200b51e272b
Soung Rong <soungcoding@gmail.com>
2019-08-26 13:47:03 +0800

    Parse search response into a user friendly format

--- price_app/static/form.js
+++ price_app/static/form.js
@@ -29,7 +29,7 @@ function formSubmission() {
     request.open("POST", "/", true);
     request.onload = function (oEvent) {
         if (request.status == 200) {
-			formResponse.innerHTML = request.response;
+            readableResponse(JSON.parse(request.response))
         } else {
             formResponse.innerHTML = "Error " + request.status + " occurred when trying to process your submission.<br \/>";
         }
@@ -40,18 +40,64 @@ function formSubmission() {
     return request
 }
 
-// JSON.parse(b.response)
-// JSON.stringify(Object.fromEntries(formData))
-// Object.fromEntries(formData)
+function readableResponse(response) {
+    if (response.search_type == 'closest_town_match') {
+        closestTownMatch(response)
+    } else if (response.search_type == 'closest_town_loose_match') {
+        closestTownLooseMatch(response)
+    }
+}
+
+function closestTownMatch(response) {
+    return formResponse.innerHTML = `${parseDistance(response)} ${parseCount(response)} ` +
+                                    `${parsePrice(response)}`
+}
+
+function closestTownLooseMatch(response) {
+    return formResponse.innerHTML = `We couldn't find a match with your exact search ` +
+                                    `criteria, so we removed some of the optional requirements.` +
+                                    `${parseDistance(response)} ${parseCount(response)} ` +
+                                    `${parsePrice(response)}`
+}
+
+function parseDistance(response) {
+    // search_distance is in meters
+    if (response.search_distance >= 1000) {
+        return `The closest area that we found a listing for, is ${response.town}.`
+    } else {
+        return `We've found a listing within the area of ${response.town}.`
+    }
+}
+
+function parseCount(response) {
+    if (response.count > 1) {
+        return `We found more than one listing for your search criteria, ` +
+               `so we took an average of the listings.`
+    } else {
+        return `We only found one listing with your search criteria, ` +
+               `so it may be harder to find a similar offer.`
+    }
+}
+
+function parsePrice(response) {
+    if (response.count > 1) {
+        return `The listed average price is ${response.price} and price per sq/ft ` +
+               `is ${response.price_per_sq_ft}.`
+    } else {
+        return `The listed price is ${response.price}, and price per sq/ft ` +
+               `is ${response.price_per_sq_ft}.`
+    }
+}
+
 
 form.addEventListener('submit', function (event) {
-	formSubmission()
-    event.preventDefault();
+    formSubmission()
+    event.preventDefault()
 }, false);
 
-form.addEventListener('keypress', function(event) {
-	if (event.key === 'Enter') {
-		formSubmission()
-		event.preventDefault();
-	}
-}, false);
+// form.addEventListener('keypress', function(event) {
+// 	if (event.key === 'Enter') {
+// 		formSubmission()
+// 		event.preventDefault();
+// 	}
+// }, false);
