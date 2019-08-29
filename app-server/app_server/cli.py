d7b8f481387fb1d97313cf4b2b608ade9bcb47bf
Soung Rong <soung@soungrong.com>
2019-08-29 15:06:20 +0800

    Remove deprecated SQL models/commands/scripts

--- app-server/app_server/cli.py
+++ app-server/app_server/cli.py
@@ -24,17 +24,3 @@ def csv_to_pandas_command(file_path, delimiter, quotechar, save):
     elif save == 'mongo':
         save_result = csv_to_pandas.save_to_mongo(dataframe)
         click.echo('Saved to mongo. {} records created.'.format(len(save_result.inserted_ids)))
-
-
-# @click.command('csv-to-sql')
-# @click.argument("file_path")
-# @click.option('--delimiter', default=',', help='CSV delimiter')
-# @click.option('--quotechar', default='"', help='CSV quotechar')
-# @with_appcontext
-# def csv_to_sql_command(file_path, delimiter, quotechar):
-#     """
-#     Create SQL records with a CSV file of property listings.
-#     """
-#     click.echo('Processing {}'.format(file_path))
-#     csv_to_sql.process_csv(file_path, delimiter, quotechar)
-#     click.echo('Processing complete.')
