c808b36624331a19c25084bcca978e7ef782a1ef
Soung Rong <soung@soungrong.com>
2019-08-29 15:10:20 +0800

    Fix generate static command

--- app-server/app_server/scripts/generate_static.py
+++ app-server/app_server/scripts/generate_static.py
@@ -3,10 +3,8 @@ from flask_frozen import Freezer
 from app_server import create_app
 
 
-app = create_app()
-
-# https://pythonhosted.org/Frozen-Flask/
-freezer = Freezer(app)
-
-if __name__ == '__main__':
+def freeze():
+    app = create_app()
+    # https://pythonhosted.org/Frozen-Flask/
+    freezer = Freezer(app)
     freezer.freeze()
